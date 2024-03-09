b447cbf11c5810630680411eb0d1a46eba741b72
tianfengli <istianfengl@gmail.com>
2024-03-09 23:40:56 +0800

    third commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Tiangfeng/puppy
 
 go 1.21.4
+
+require github.com/Tiangfeng/dog v0.0.0-20240309153726-761a5880b363 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,17 @@
 package puppy
 
+import (
+	"github.com/Tiangfeng/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Bark())
 }
